04e26aae118d1cfa3ec430a4163100bcd4fa69f4
Chris Pyles <cpyles@berkeley.edu>
2019-07-10 11:05:33 -0700

    fixed qgrid_bridge issue

--- lab05/qgrid_bridge.py
+++ lab05/qgrid_bridge.py
@@ -16,7 +16,7 @@ def create_table(num_rows, num_cols, names):
         
 def generate_grid(num_rows, num_cols, names, preset_values=None):
     global num_grids
-    assert type(preset_values) == dict, "preset_values must be a dict"
+    assert type(preset_values) == dict or preset_values == None, "preset_values must be a dict"
     create_table(num_rows, num_cols, names)
     df = pd.read_csv("data/empty-grid-{}.csv".format(num_grids))
     if preset_values:
